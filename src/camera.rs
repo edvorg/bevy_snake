e7313766a01ac989ba13ec4ceb568090a149b9ee
Eduard Knyshov <edvorg@gmail.com>
2024-09-29 17:32:55 +0700

    adjust camera position

--- src/camera.rs
+++ src/camera.rs
@@ -14,7 +14,7 @@ fn setup(mut commands: Commands) {
     // Camera
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 10.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
+            transform: Transform::from_xyz(0.0, 25.0, 0.0).looking_at(Vec3::ZERO, Vec3::Z),
             camera: Camera {
                 hdr: true,
                 ..Default::default()
