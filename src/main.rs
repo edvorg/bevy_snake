3d02be79ea3ac7be23bac0dc611d3881e6dbf641
Eduard Knyshov <edvorg@gmail.com>
2024-09-30 00:31:35 +0700

    add WASM build support

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,16 @@ use bevy::render::settings::{Backends, RenderCreation, WgpuSettings};
 use bevy::render::RenderPlugin;
 
 fn main() {
+    #[cfg(target_arch = "wasm32")]
+    const BACKENDS: Backends = Backends::GL;
+    #[cfg(not(target_arch = "wasm32"))]
+    const BACKENDS: Backends = Backends::VULKAN;
+
     App::new()
         .insert_resource(ClearColor(BLACK.into()))
         .add_plugins(DefaultPlugins.set(RenderPlugin {
             render_creation: RenderCreation::Automatic(WgpuSettings {
-                backends: Some(Backends::VULKAN),
+                backends: Some(BACKENDS),
                 ..default()
             }),
             ..default()
